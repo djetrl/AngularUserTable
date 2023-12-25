8f5d5c1775a2235ec960d9feda08ec5f7e50d96a
djetrl <leshadjet@mail.ru>
2023-12-26 01:09:00 +0700

    deploy-first version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormAddUserComponent } from './form-add-user/form-add-user.component';
+import { TableComponent } from './table/table.component';
+import { Routes, RouterModule, RouterOutlet } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import { ApiRequestsService } from './Service/ApiRequests/api-requests.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+const appRoutes:Routes=[
+  {path:'', component:AppComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormAddUserComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    RouterOutlet, 
+    RouterModule, 
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxPaginationModule
   ],
-  providers: [],
+  providers: [ApiRequestsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
